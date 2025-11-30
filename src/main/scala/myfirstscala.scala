5b42522d2db94f326a90e4bdacfb15fa5980fb6d
RaymondCivis <19010461@imail.sunway.edu.my>
2025-11-30 22:01:11 +0800

    Removed println statement

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -6,7 +6,6 @@ object MyApp extends JFXApp3:
   override def start(): Unit =
     stage = new PrimaryStage()
 
-  @main def Hello() = println("Testing Commit to Project Assignment 2")
 
 
 end MyApp
