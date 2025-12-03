ffadcf5369da481858ca1ff5e692bb27888e9806
RaymondCivis <19010461@imail.sunway.edu.my>
2025-12-04 00:39:15 +0800

    Removed (initial test) lines of code. Saving local state before pull.

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -3,9 +3,12 @@ import scalafx.application.JFXApp3.PrimaryStage
 
 object MyApp extends JFXApp3:
 
-  override def start(): Unit =
-    stage = new PrimaryStage()
 
+
+
+  override def start(): Unit = {
+    stage = new PrimaryStage()
+  }
 
 
 end MyApp
